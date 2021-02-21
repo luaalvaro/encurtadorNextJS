0540be6b012f58f9a3b36b2ee2bc5944cdf5cca7
Luã Álvaro <luan.alc@hotmail.com>
2021-02-21 14:48:16 -0300

    front end e back end finalizados, falta integrar os dois

--- pages/api/[shortId].js
+++ pages/api/[shortId].js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
                 })
 
                 if (!short) {
-                    return res.status(400).json({ success: false })
+                    return res.status(400).json({ success: false, message: "This shortIdOld not exists" })
                 }
 
                 res.status(200).json({ success: true, data: short })
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,36 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
+
+var validUrl = require('valid-url');
 
 export default function Home() {
+
+  let [urlDest, setUrlDest] = useState('')
+
+  function createShortener() {
+
+    const input = document.getElementById('inputField')
+    const inputText = document.getElementById('inputText')
+
+    if (validUrl.isUri(urlDest)) {
+      input.classList.remove('border-red-500')
+      input.classList.add('border-green-500')
+      inputText.classList.remove('text-red-500')
+
+      const data = {
+        shortId: nanoid(6),
+        urlDest: urlDest
+      }
+      console.log(data)
+    } else {
+      input.classList.add('border-red-500')
+      input.classList.remove('border-gray-400')
+      inputText.classList.add('text-red-500')
+    }
+  }
+
   return (
     <>
       <Head>
@@ -12,11 +41,11 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-col items-center justify-center w-10/12 h-screen mx-auto">
-        <Image src="/pie-chart.svg" width="196" height="196" />
+        <Image src="/logo.svg" width="196" height="196" />
         <h1 className="mt-4 text-3xl">URL SHORTENER</h1>
-        <div className="w-full mt-8 bg-gray-200 border border-gray-400 rounded rounded-l md:max-w-3xl">
-          <input className="w-8/12 p-3 bg-gray-200 rounded-l focus:outline-none focus:text-green-600 focus:bg-gray-100" type="text" />
-          <button className="w-4/12 p-3 font-bold text-white bg-green-500 rounded-r focus:outline-none hover:bg-green-600">SHORT</button>
+        <div id="inputField" className="w-full mt-8 bg-gray-200 border border-gray-400 rounded rounded-l md:max-w-3xl">
+          <input id="inputText" placeholder="http://www." value={urlDest} onChange={(e) => setUrlDest(urlDest = e.target.value)} autoComplete="off" className="w-8/12 p-3 bg-gray-200 rounded-l focus:outline-none focus:text-green-600 focus:bg-white" type="text" />
+          <button onClick={createShortener} id="btn" className="w-4/12 p-3 font-bold text-white bg-green-500 rounded-r focus:outline-none hover:bg-green-600">SHORT</button>
         </div>
         <p className="mt-20 text-green-800 opacity-80">Enjoy, completely free, forever!</p>
       </div>
